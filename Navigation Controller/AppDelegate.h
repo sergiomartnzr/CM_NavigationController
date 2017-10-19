95c66826dc8ac520acb653b019740c5a6d0dbdf6
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2017-10-18 19:42:33 -0500

    Initial Commit

---
+++ Navigation Controller/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Navigation Controller
+//
+//  Created by Walter Gonzalez Domenzain on 18/10/17.
+//  Copyright © 2017 wgdomenzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Navigation Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Navigation Controller
+//
+//  Created by Walter Gonzalez Domenzain on 18/10/17.
+//  Copyright © 2017 wgdomenzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
